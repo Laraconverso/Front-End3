a3efef4ffe47fa42216e8439226f50d579978c51
Lara Converso <laraconverso@gmail.com>
2022-08-08 22:04:07 -0300

    4th class

--- Proyecto/digital-house/src/App.js
+++ Proyecto/digital-house/src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          HELLO WORLD FROM REACT!
+        </h1>
+        <img src="https://github.com/Ivanszs/dh-frontend3-clases/blob/master/clase-4/satisfied.png?raw=true" className="image" alt="happy man"/>
       </header>
     </div>
   );
