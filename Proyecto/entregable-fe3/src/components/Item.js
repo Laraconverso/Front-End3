a69350e963fd0b7271d242ad2761fe91a6ee89a7
Lara Converso <laraconverso@gmail.com>
2022-08-29 21:53:15 -0300

    updated entrega

--- Proyecto/entregable-fe3/src/components/Item.js
+++ Proyecto/entregable-fe3/src/components/Item.js
@@ -11,22 +11,23 @@
 import { useState } from "react";
 
 
-export default function Item(props) {
+export default function Item({name, description, stock,
+  addItem}) {
 
-  const [cantStock, setCantStock] = useState(props.stock);
+  const [cantStock, setCantStock] = useState(stock);
 
   function comprar(){
-    props.addItem();
+    addItem();
     setCantStock((cant)=> cant - 1);
     console.log("Se resto uno de la cant. en stock.");
   }
 
   return (
     <div className='producto'>
-      <h3>{props.name}</h3>
-      <p>{props.description}</p>
+      <h3>{name}</h3>
+      <p>{description}</p>
       <h5>En Stock: {cantStock > 0 ? cantStock : <span>Agotado</span>} </h5>
-      <button disabled= {cantStock <=0} onClick={comprar}>{cantStock > 0 ? 'Comprar' : 'Sin Stock'}</button>
+      <button disabled={cantStock <=0} onClick={comprar}>{cantStock > 0 ? 'Comprar' : 'Sin Stock'}</button>
     </div>
   )
 }
